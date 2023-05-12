d564da12d8b239874810602bb0fdc936b43d6cfa
Ciromontes <ciromontes25@hotmail.com>
2023-05-12 08:23:57 -0500

    agregando columnas status y fecha por defecto

--- src/main/java/foro/alura/api1/topico/DatosRegistroTopico.java
+++ src/main/java/foro/alura/api1/topico/DatosRegistroTopico.java
@@ -1,17 +1,25 @@
-package foro.alura.api1.topico
-        ;
+package foro.alura.api1.topico;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+import java.time.LocalDateTime;
 
 public record DatosRegistroTopico(
         @NotBlank
         String titulo,
         @NotBlank
         String mensaje,
-
+        String status,
         @NotBlank
         String autor,
         @NotNull
-        Curso curso) {
+        Curso curso,
+
+        LocalDateTime fecha)
+
+
+
+{
 }
--- src/main/java/foro/alura/api1/topico/Topico.java
+++ src/main/java/foro/alura/api1/topico/Topico.java
@@ -6,6 +6,9 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.print.attribute.standard.DateTimeAtCompleted;
+import java.time.LocalDateTime;
+
 @Table(name ="topicos2")
 @Entity(name = "Topico")
 @Getter
@@ -18,16 +21,28 @@ public class Topico {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
+    private String status = "pendiente"; // inicializamos con el valor por defecto
     private String mensaje;
+    private LocalDateTime fecha = LocalDateTime.now(); // inicializamos con el valor por defecto
+
     private String autor;
+
 
     @Enumerated(EnumType.STRING)
     private Curso curso;
+
 
     public Topico(DatosRegistroTopico datosRegistroTopico) {
         this.titulo = datosRegistroTopico.titulo();
         this.mensaje = datosRegistroTopico.mensaje();
         this.autor = datosRegistroTopico.autor();
         this.curso = datosRegistroTopico.curso();
+        if (datosRegistroTopico.status() != null && !datosRegistroTopico.status().isEmpty()) {
+            this.status = datosRegistroTopico.status();
+        }
+        if (datosRegistroTopico.fecha() != null) {
+            this.fecha = datosRegistroTopico.fecha();
+
+        }
     }
 }
