2b0d671e8dd3cbe5b0597ab642e4ff4e7ebd0450
Ciromontes <ciromontes25@hotmail.com>
2023-05-22 20:51:37 -0500

    Control de acceso obteniendo y validando Token

--- src/main/java/foro/alura/api1/infra/security/SecurityConfigurations.java
+++ src/main/java/foro/alura/api1/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package foro.alura.api1.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,21 +11,34 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+    @Autowired
+    private SecuriyFilter securiyFilter;
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf().disable().sessionManagement()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().build();
+                .and().authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilterBefore(securiyFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
     }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-        throws Exception {
+            throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
     @Bean
     public BCryptPasswordEncoder PaswordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/java/foro/alura/api1/infra/security/TokenService.java
+++ src/main/java/foro/alura/api1/infra/security/TokenService.java
@@ -3,6 +3,8 @@ package foro.alura.api1.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import foro.alura.api1.domain.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,33 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw new RuntimeException();
         }
+    }
+    public String getSubject(String token) {
+        if(token == null){
+            throw new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    // specify an specific claim validations
+                    .withIssuer("foro alura")
+                    // reusable verifier instance
+                    .build()
+
+                    .verify(token);
+            verifier.getSubject();
+
+            //decodedJWT = verifier.verify(token);
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+            // Invalid signature/claims
+        }
+        if(verifier.getSubject() ==null) {
+            throw new RuntimeException("Verifier invalido");
+        }
+
+        return verifier.getSubject();
     }
     private Instant generarFechaExpiracion(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
